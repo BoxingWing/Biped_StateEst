3846d248bf684e4b8dcfeb33dfec67624eda12a3
BoxingWang <1182602774@qq.com>
2022-12-01 22:50:04 +0800

    add class

---
+++ StateEst.cpp
@@ -0,0 +1,5 @@
+//
+// Created by boxingwang on 22-12-1.
+//
+
+#include "StateEst.h"
---
+++ StateEst.h
@@ -0,0 +1,14 @@
+//
+// Created by boxingwang on 22-12-1.
+//
+
+#ifndef BIPED_STATEEST_STATEEST_H
+#define BIPED_STATEEST_STATEEST_H
+
+
+class StateEst {
+
+};
+
+
+#endif //BIPED_STATEEST_STATEEST_H
